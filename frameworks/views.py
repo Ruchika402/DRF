c1987e885929c065722008e7929192c9697a9ac3
Ruchika Adak <adakruchika5@gmail.com>
2026-05-20 14:55:38 +0530

    learning the serializers and models serializers

--- frameworks/views.py
+++ frameworks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Student
+from .serializers import StudentSerializer
+
+
+@api_view(['GET'])
+def student_list(request):
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
--- myproject/settings.py
+++ myproject/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest frameworks'
+
+    'rest_framework',
     'frameworks',
 ]
+ 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
