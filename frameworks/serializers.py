f8757bd27454eab5e2da7505214044e660e117ae
Ruchika Adak <adakruchika5@gmail.com>
2026-05-29 21:05:05 +0530

    modified the serializers file

--- frameworks/serializers.py
+++ frameworks/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Student,Teacher
+from .models import Student,Teacher,Subject
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta :
@@ -10,3 +10,8 @@ class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = '__all__'
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields='__all__'
