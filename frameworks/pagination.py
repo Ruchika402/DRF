463f561d96478df45d6795ec8208a28bd4d0260d
Ruchika Adak <adakruchika5@gmail.com>
2026-06-07 17:27:35 +0530

    add pagination in drf means divide the data into pages

--- frameworks/pagination.py
+++ frameworks/pagination.py
@@ -3,3 +3,8 @@ from rest_framework.pagination import PageNumberPagination
 class StudentPagination(PageNumberPagination):
     page_size = 3
     page_size_query_param = 'size'
+    #ordering = 'id'  ordering pagination
+
+#limit offset pagination
+#class StudentPagination(LimitOffsetPagination):
+   # default_limit = 3
